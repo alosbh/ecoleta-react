aeff940bd9d5d6486657cc3ff0d99c9492e87de4
Andre <alosbhgmail.com>
2020-08-28 20:25:49 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+import './App.css';
+
+import Routes from './routes'
+import Home from './pages/Home'
+import CreatePoint from './pages/CreatePoint'
+function App() {
+  return (
+
+          <Routes/>
+
+  );
+}
+
+export default App;
